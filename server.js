9c5821364e31038f1ae0420d31db5db75456fb1a
Leonel Rosales <rleonel426@gmail.com>
2020-01-27 21:11:19 -0800

    more dependencies

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const app = express();
 
 //define a route directly on the app
 app.get('/', function (req, res) {
-    res.send('<h2>Hello world</h2>');
+    res.send('<h2>Hello Express</h2>');
 });
+//configure the app (app.set)
 
+//mount midleware (app.use)
 
+//mount routes
 
+//tell the app to listen on port 3000
 app.listen(3000, function(){
     console.log('Listening on port 3000')
 });

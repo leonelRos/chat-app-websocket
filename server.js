c4b8bfcf43cc320fd630b9a0ac27b7d01cac8d88
Leonel Rosales <rleonel426@gmail.com>
2020-01-27 22:29:56 -0800

    socketio implemented

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 //require modules
 const express = require('express');
 const path = require('path');
+const io = require('socket.io')();
 //create express app
 const app = express();
 
 //define a route directly on the app
+app.get('/', function(req,res){
+    res.send(__dirname + '/home');
+})
+
+io.on('connection', function(socket) {
+    socket.emit('news', {hello: 'world'});
+    socket.on('my other event', function (data){
+        console.log(data);
+    });
+});
+
 app.get('/', function (req, res) {
     res.send('<h2>Hello Express</h2>');
 });
@@ -20,6 +32,7 @@ app.set('views', path.join(__dirname, 'views'));
 //mount routes
 
 //tell the app to listen on port 3000
+
 app.listen(3000, function(){
     console.log('Listening on port 3000')
 });

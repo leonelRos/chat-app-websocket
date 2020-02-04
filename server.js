bfcb53a7eb2ddd142921f4fa9e830e36f59f4fed
Leonel Rosales <rleonel426@gmail.com>
2020-02-03 23:58:24 -0800

    fix mongoose connection

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 //require modules
 const express = require('express');
+const path = require("path")
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
 //create express app
 const app = express();
-
+require('dotenv').config();
+require('/')
+const db = mongoose.connection;
+const port = process.env.PORT || 3000;
 //define a route directly on the app
-
 app.use(express.static(__dirname));
-
-
 
 
 //configure the app (app.set)
@@ -18,7 +25,10 @@ app.use(express.static(__dirname));
 //mount routes
 
 //tell the app to listen on port 3000
+db.once('connected', () => {
+    console.log(`connected to MongoDB ${db.name} at ${db.host}:${db.port}` )
+});
 
-app.listen(3000, function(){
-    console.log('Listening on port 3000')
+app.listen(port, function(){
+    console.log(`Express listening on port ${port}`)
 });

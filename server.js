e16d40dcbc6d0ff4a3c880faca7b7c62325f461a
Leonel Rosales <rleonel426@gmail.com>
2020-02-03 22:49:03 -0800

    installing cdn

--- server.js
+++ server.js
@@ -1,32 +1,18 @@
 //require modules
 const express = require('express');
-const path = require('path');
-const io = require('socket.io')();
+
 //create express app
 const app = express();
 
 //define a route directly on the app
-app.get('/', function(req,res){
-    res.send(__dirname + '/home');
-})
 
-io.on('connection', function(socket) {
-    socket.emit('news', {hello: 'world'});
-    socket.on('my other event', function (data){
-        console.log(data);
-    });
-});
+app.use(express.static(__dirname));
 
-app.get('/', function (req, res) {
-    res.send('<h2>Hello Express</h2>');
-});
 
-app.get('/home', function(req, res){
-    res.render('home');
-});
+
+
 //configure the app (app.set)
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'));
+
 //mount midleware (app.use)
 
 //mount routes
